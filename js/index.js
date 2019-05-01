47659c401d84b6f447d4a52d86d86e83bc299058
Kevin Grant <ConsciousK@yahoo.com>
2019-05-01 19:46:34 -0400

    Saving current progress.

--- js/index.js
+++ js/index.js
@@ -48,9 +48,33 @@ navitems[2].textContent = 'Vision'
 navitems[3].textContent = 'Features'
 navitems[4].textContent = 'About'
 navitems[5].textContent = 'Contact'
+
 let headerimage = document.getElementById('cta-img')
 headerimage.setAttribute('src', siteContent['cta']['img-src'])
+
 let mainheader = document.querySelector('h1')
 mainheader.innerHTML = "DOM"+ `<br />` +"Is"+ `<br />` + "Awesome"
+
 let buttontext = document.querySelector('button')
 buttontext.textContent = 'Get Started'
+
+let topheaders = document.querySelectorAll('.text-content h4')
+topheaders[0].textContent = 'Features'
+topheaders[1].textContent = 'About'
+
+let topcontent = document.querySelectorAll('.top-content .text-content p')
+topcontent[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+topcontent[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+let centerimage = document.getElementById("middle-img")
+centerimage.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+let bottomheaders = document.querySelectorAll('.bottom-content h4')
+bottomheaders[0].textContent = 'Services'
+bottomheaders[1].textContent = 'Product'
+bottomheaders[2].textContent = 'Vision'
+
+let bottomcontent = document.querySelectorAll('.bottom-content .text-content p')
+bottomcontent[0].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+bottomcontent[1].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+bottomcontent[2].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
